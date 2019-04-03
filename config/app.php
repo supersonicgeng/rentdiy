e7d2c41e231af7c7fd6723af27c13b01474a0dfc
supersonicgeng <13971394623@163.com>
2019-04-03 19:03:11 +0800

    vue 页面上传

--- config/app.php
+++ config/app.php
@@ -180,7 +180,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Intervention\Image\ImageServiceProvider::class, //图片处理插件
-        
+
 
         Zizaco\Entrust\EntrustServiceProvider::class,//RBAC权限管理
 
