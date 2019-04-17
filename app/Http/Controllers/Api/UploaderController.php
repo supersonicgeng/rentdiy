8936d2ff80a61707f2ffd0b13c8c73da2298b434
supersonicgeng <13971394623@163.com>
2019-04-17 10:07:32 +0800

    调试图片上传

--- app/Http/Controllers/Api/UploaderController.php
+++ app/Http/Controllers/Api/UploaderController.php
@@ -28,7 +28,7 @@ class UploaderController extends CommonController
         $save_path               = storage_path(Img::$SAVE_PATH).$dir . date('Y-m-d');
         $storage_file_name       = md5_file($file->getRealPath());
         $storage_file_name_thumb = 'thumb_'.$storage_file_name;
-        config(['upload.driver' => 'oss']);
+        //config(['upload.driver' => 'oss']);
         if(config('upload.driver') == 'local'){
             if ($file->move($save_path, $storage_file_name . '.' . $ext)) {
                 Image::make($save_path . DIRECTORY_SEPARATOR . $storage_file_name . '.' . $ext)->resize(100, null, function ($constraint) {
@@ -48,7 +48,7 @@ class UploaderController extends CommonController
             $oss->uploadFile($dir.$storage_file_name . '.' . $ext, $file->getRealPath(),['ContentType' => $file->getMimeType()]);
             //$url = $oss->getPublicUrl($dir.$storage_file_name . '.' . $ext, $file->getRealPath(),['ContentType' => $file->getMimeType()]);
             $dirs =  substr($dir,0,strlen($dir)-1);
-            $url = config('upload.oss.imgUrl').'/'.$dirs.'%5C'.$storage_file_name.'.'.$ext;
+            $url = config('upload.oss.imgUrl').'/'.$dirs.'/'.$storage_file_name.'.'.$ext;
             $data = [
                 'img'       => $url,
             ];
