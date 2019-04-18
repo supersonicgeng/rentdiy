4c65f0a6e76038c715497d0e72ab82bab9b8cca4
supersonicgeng <13971394623@163.com>
2019-04-18 11:13:52 +0800

    租户信息增加zipcode字段

--- app/Model/Tenement.php
+++ app/Model/Tenement.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Tenement extends Model
 {
     protected $table    = 'tenement_information';
-    protected $fillable = ['id','user_id','tenement_id','first_name','middle_name','last_name','mobile','phone','email','birthday','mail_address','service_address','mail_code','bank_no','headimg',
+    protected $fillable = ['id','user_id','tenement_id','first_name','middle_name','last_name','mobile','phone','email','birthday','mail_address','service_address','mail_code','zip_code','bank_no','headimg',
         'contact_phone','contact_address','company','job_title','instruction','created_at','updated_at','deleted_at'];
 }
