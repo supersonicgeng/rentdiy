96e8ebf3e39bd2d42b52413831f3c2571e167d74
supersonicgeng <13971394623@163.com>
2019-04-18 19:35:08 +0800

    调试结构

--- app/Services/HouseService.php
+++ app/Services/HouseService.php
@@ -425,7 +425,7 @@ class HouseService extends CommonService
     {
         $model = new RentHouse();
         $rent_house_id = $input['rent_house_id'];
-        $res = $model->where('id',$rent_house_id)->select('group_id','property_name','rent_fee_pre_week','building_area','actual_area','pre_rent','least_rent_time','margin_rent','bedroom_no','bathroom_no','parking_no','garage_no','require_renter','short_words','rent_fee','rent_least_fee','can_party','can_pet','can_smoke','other_rule','address','lat','lon','available_date')->get()->toArray();
+        $res = $model->where('id',$rent_house_id)->select('group_id','property_name','rent_fee_pre_week','building_area','actual_area','pre_rent','least_rent_time','margin_rent','bedroom_no','bathroom_no','parking_no','garage_no','require_renter','short_words','rent_fee','rent_least_fee','can_party','can_pet','can_smoke','other_rule','address','lat','lon','available_date')->first()->toArray();
         if($res){
             $res['house_pic'] =  RentPic::where('rent_house_id',$rent_house_id)->where('deleted_At',null)->pluck('house_pic')->toArray();
             //$res['short_words'] = explode(',',$res['short_words']);
