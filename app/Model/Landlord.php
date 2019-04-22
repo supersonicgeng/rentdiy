481d369c4f84a73fd3bf9977f23fd9df6fc6a200
supersonicgeng <13971394623@163.com>
2019-04-22 11:24:36 +0800

    调整字段

--- app/Model/Landlord.php
+++ app/Model/Landlord.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Landlord extends Model
 {
     protected $table    = 'landlord_information';
-    protected $fillable = ['id','user_id','landlord_sn','landlord_name','property_address','first_name','middle_name','last_name','headimg','assign_name','tax_no','phone','tel','email',
+    protected $fillable = ['id','user_id','landlord_sn','landlord_name','property_address','first_name','middle_name','last_name','headimg','assign_name','tax_no','phone','mobile','email',
         'bank_account','maill_address','mail_code','notice','created_at','updated_at','deleted_at'];
 }
