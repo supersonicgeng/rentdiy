a62128dfbbc0c6003d771e23ba7cce31985e1387
supersonicgeng <13971394623@163.com>
2019-04-22 11:58:57 +0800

    调试路由

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Routing\Router;
 /*Router::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-Route::post('/imageUploader', 'Api\UploaderController@imageUploader');
+Route::post('/imageUploader', 'Api\UploaderController@imageUploader'); // 接口 done
 Route::post('/fileUploader', 'Api\UploaderController@fileUploader'); // 文件上传 4.17
 Route::post('/imageUploaderBase64', 'Api\UploaderController@imageUploaderBase64');
 Route::any('/address', 'Api\AddressController@address');
@@ -34,23 +34,23 @@ Route::get('/system/rewardRate', 'Api\PublicController@rewardRate');
 Route::group(['namespace' => 'Api'], function (Router $router) {
     // 用户模块
     $router->group(['prefix' => 'user'], function (Router $router){
-        $router->post('userRegister','UserController@userRegister'); // 用户注册
-        $router->post('userLogin','UserController@userLogin'); // 用户登录
-        $router->post('changePassword','UserController@changePassword'); // 修改密码
-        $router->post('forgetPassword','UserController@forgetPassword'); // 找回密码
+        $router->post('userRegister','UserController@userRegister'); // 用户注册 接口done
+        $router->post('userLogin','UserController@userLogin'); // 用户登录 接口done
+        $router->post('changePassword','UserController@changePassword'); // 修改密码 接口done
+        $router->post('forgetPassword','UserController@forgetPassword'); // 找回密码 接口done
     });
     // 公共模块
     $router->group(['prefix' => 'public'], function (Router $router){
-        $router->post('sendVerify','PublicController@sendVerify'); // 发送验证码
-        $router->post('checkVerify','PublicController@checkVerify'); // 验证验证码
-        $router->post('getRegion','PublicController@getRegion'); // 获得州地址
-        $router->post('getTa','PublicController@getTa'); // 获得市地址
-        $router->post('getDistrict','PublicController@getDistrict'); // 获得地区地址
+        $router->post('sendVerify','PublicController@sendVerify'); // 发送验证码 接口done
+        $router->post('checkVerify','PublicController@checkVerify'); // 验证验证码 接口done
+        $router->post('getRegion','PublicController@getRegion'); // 获得州地址 接口done
+        $router->post('getTa','PublicController@getTa'); // 获得市地址 接口done
+        $router->post('getDistrict','PublicController@getDistrict'); // 获得地区地址 接口done
     });
     // 租赁市场
     $router->group(['prefix' => 'house'], function (Router $router){
-        $router->post('houseList', 'HouseController@getHouseList'); // 房屋主档列表
-        $router->post('houseDetail','HouseController@houseDetail'); // 房屋主档具体信息
+        $router->post('houseList', 'HouseController@getHouseList'); // 房屋主档列表  // 接口 done
+        $router->post('houseDetail','HouseController@houseDetail'); // 房屋主档具体信息 // 接口 done
     });
     // 操作员模块
     $router->group(['prefix' => 'operator'], function (Router $router){
@@ -63,21 +63,21 @@ Route::group(['namespace' => 'Api'], function (Router $router) {
 Route::group(['namespace' => 'Api','middleware' => 'CheckLogin'], function (Router $router) {
     // 用户模块
     $router->group(['prefix' => 'user'], function (Router $router){
-        $router->post('getUserRoleId','UserController@getUserRoleId'); // 用户获得各角色下的角色id 3.27
-        $router->post('becomeLandlord','UserController@becomeLandlord'); // 成为房东 4.16
-        $router->post('becomeProviders','UserController@becomeProviders'); // 成为服务商 4.16
-        $router->post('becomeTenement','UserController@becomeTenement'); // 成为租客 4.16
-        $router->post('updateHeadImg','UserController@updateHeadImg'); // 更新头像 4.17
-        $router->post('addPhone','UserController@addPhone'); // 增加手机 4.17
-        $router->post('addEmail','UserController@addEmail'); // 增加邮箱 4.17
+        $router->post('getUserRoleId','UserController@getUserRoleId'); // 用户获得各角色下的角色id 3.27 // 接口done
+        $router->post('becomeLandlord','UserController@becomeLandlord'); // 成为房东 4.16 // 接口done
+        $router->post('becomeProviders','UserController@becomeProviders'); // 成为服务商 4.16 // 接口done
+        $router->post('becomeTenement','UserController@becomeTenement'); // 成为租客 4.16 // 接口done
+        $router->post('updateHeadImg','UserController@updateHeadImg'); // 更新头像 4.17 // 接口done
+        $router->post('addPhone','UserController@addPhone'); // 增加手机 4.17 //接口done
+        $router->post('addEmail','UserController@addEmail'); // 增加邮箱 4.17  // 接口done
     });
     //房屋主档系统
     $router->group(['prefix' => 'house'], function (Router $router) {
-        $router->post('addHouseList', 'HouseController@addHouseList'); // 添加房屋主档
-        $router->post('houseListPut','HouseController@houseListPut'); // 房屋主档上架 3.21
-        $router->post('getSelfHouseList','HouseController@getSelfHouseList'); // 获得房屋主档信息列表 3.25
+        $router->post('addHouseList', 'HouseController@addHouseList'); // 添加房屋主档 // 接口done
+        $router->post('houseListPut','HouseController@houseListPut'); // 房屋主档上架 3.21 // 接口done
+        $router->post('getSelfHouseList','HouseController@getSelfHouseList'); // 获得房屋主档信息列表 3.25 // 接口done
         $router->post('getHouseGroupDetail','HouseController@getHouseGroupDetail'); // 获得房屋主档信息 房东编辑用 3.25
-        $router->post('editHouseList', 'HouseController@editHouseList'); // 修改房屋主档 3.22
+        $router->post('editHouseList', 'HouseController@editHouseList'); // 修改房屋主档 3.22 // 接口done
         $router->post('deleteHouseList', 'HouseController@deleteHouseList'); // 删除房屋主档 3.27
     });
     // 租房系统
@@ -97,14 +97,14 @@ Route::group(['namespace' => 'Api','middleware' => 'CheckLogin'], function (Rout
     });
     // 租户系统
     $router->group(['prefix' => 'tenement'], function (Router $router) {
-        $router->post('addTenementInformation', 'TenementController@addTenementInformation'); // 添加租户信息 3.21
-        $router->post('getTenementSelfInformation', 'TenementController@getTenementSelfInformation'); // 获得租户信息 3.22
-        $router->post('editTenementInformation', 'TenementController@editTenementInformation'); // 修改租户信息 3.22
+        $router->post('addTenementInformation', 'TenementController@addTenementInformation'); // 添加租户信息 3.21 接口done
+        $router->post('getTenementSelfInformation', 'TenementController@getTenementSelfInformation'); // 获得租户信息 3.22 接口done
+        $router->post('editTenementInformation', 'TenementController@editTenementInformation'); // 修改租户信息 3.22 接口done
         $router->post('deleteTenementInformation', 'TenementController@deleteTenementInformation'); // 删除租户信息 3.27
     });
     // 房东管理
     $router->group(['prefix' => 'landlord'], function (Router $router) {
-        $router->post('addLandlordInformation', 'LandlordController@addTenementInformation'); // 添加房东联系人 3.21
+        $router->post('addLandlordInformation', 'LandlordController@addLandlordInformation'); // 添加房东联系人 3.21
         $router->post('getLandlordList','LandlordController@getLandlordList'); // 房东获得当前已经存入的房东联系人列表 3.22
         $router->post('getLandlordInformation', 'LandlordController@getTenementInformation'); // 获得房东联系人信息 3.22
         $router->post('editLandlordInformation', 'LandlordController@editTenementInformation'); // 修改房东联系人 3.22
