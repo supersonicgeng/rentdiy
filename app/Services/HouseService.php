9297379aaef73f4655b0f3a7fcef109a6333fbbe
supersonicgeng <13971394623@163.com>
2019-04-25 16:09:43 +0800

    调试格式

--- app/Services/HouseService.php
+++ app/Services/HouseService.php
@@ -961,16 +961,22 @@ class HouseService extends CommonService
             'can_party','can_pet','can_smoke','other_rule','rent_method')->first();
         if($res['rent_category'] == 1 || $res['rent_category'] == 4){
             $res['contact_info'] = RentContact::where('rent_house_id',$res['rent_house_id'])->where('deleted_at',null)->get()->toArray();
-            $res['house_pic']['url'] = RentPic::where('rent_house_id',$res['rent_house_id'])->where('deleted_at',null)->pluck('house_pic')->toArray();
+            $data = RentPic::where('rent_house_id',$res['rent_house_id'])->where('deleted_at',null)->pluck('house_pic')->toArray();
+            foreach ($data as $k => $v){
+                $res['house_pic']['url'][] = $v;
+            }
             return $this->success('get house info success',$res);
         }elseif($res['rent_category'] == 2 || $res['rent_category'] == 3){
             $res['contact_info'] = RentContact::where('rent_house_id',$res['rent_house_id'])->where('deleted_at',null)->get()->toArray();
             $res['room_info'] = $model->where('user_id',$user_id)->where('group_id',$group_id)->where('deleted_at',null)->select('id as rent_house_id','group_id','bed_no','bus_station','school','supermarket',
                 'hospital','room_name','room_description','shower_room','bed_no','require_renter','room_short_words', 'rent_period','rent_least_fee','rent_fee_detail','rent_fee','rent_fee_pre_week','least_rent_time','least_rent_method','pre_rent','pre_rent_fee','margin_rent','margin_rent_fee','total_need_fee',
                 'can_party','can_pet','can_smoke','other_rule','rent_method')->get()->toArray();
-            foreach ($res['room_info'] as $k => $v){
-                $res['room_info'][$k]['room_short_words'] = explode(',',$v['room_short_words']);
-                $res['room_info'][$k]['house_pic']['url'] = RentPic::where('rent_house_id',$v['rent_house_id'])->where('deleted_at',null)->pluck('house_pic')->toArray();
+            foreach ($res['room_info'] as $key => $value){
+                $res['room_info'][$key]['room_short_words'] = explode(',',$value['room_short_words']);
+                $data= RentPic::where('rent_house_id',$value['rent_house_id'])->where('deleted_at',null)->pluck('house_pic')->toArray();
+                foreach ($data as $k => $v){
+                    $res['room_info'][$key]['house_pic']['url'][] = $v;
+                }
             }
             return $this->success('get house info success',$res);
         }else{
