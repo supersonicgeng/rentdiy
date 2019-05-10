ca711f0a4b78c87728f048145b271383f56b430c
supersonicgeng <13971394623@163.com>
2019-05-10 14:36:21 +0800

    调试

--- app/Model/Inspect.php
+++ app/Model/Inspect.php
@@ -8,5 +8,5 @@ class Inspect extends Model
 {
     protected $table    = 'inspect_list';
     protected $fillable = ['id','rent_house_id','contract_id','inspect_name','inspect_status','inspect_method','inspect_category','inspect_start_date','inspect_end_date','inspect_completed_date','inspect_note','chattel_note',
-        'created_at','updated_at','deleted_at'];
+        'check_user_id','check_operator_id','created_at','updated_at','deleted_at'];
 }
