4fdd71baa5d5e41bb53b06362d19d22d550a5ecf
supersonicgeng <13971394623@163.com>
2019-05-13 17:34:20 +0800

    调试bug

--- app/Model/Img.php
+++ app/Model/Img.php
@@ -11,6 +11,6 @@ class Img extends Model
     public static $EQRCODE = 'evaluate_qrcode_forever';  //司机评价二维码永久
     public static $EXCEL = 'excel';
     public static $IMG = '';
-    public static $IMG_EXT = ["png", "jpg", "gif","jpeg","bmp"];
+    public static $IMG_EXT = ["png", "jpg", "gif","jpeg","bmp","mp4","avi","mov","wmv","rmvb",'rm'];
     public static $FILE_EXT = ['doc','docx','pdf','xls','xlsx',"png", "jpg", "gif","jpeg","bmp","mp4","avi","mov","wmv","rmvb",'rm'];
 }
