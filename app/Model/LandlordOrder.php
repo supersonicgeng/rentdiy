3123e47933046b02fd9515d8d77092d61e14f0ae
supersonicgeng <13971394623@163.com>
2019-05-15 17:13:34 +0800

    增加字段

--- app/Model/LandlordOrder.php
+++ app/Model/LandlordOrder.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class LandlordOrder extends Model
 {
     protected $table    = 'landlord_order';
-    protected $fillable = ['id','rent_application_id','rent_contract_id','inspect_id','issue_id','group_id','jobs','user_id','tenement_id','order_sn','order_type','rent_house_id','District','TA','Region','start_time','end_time','requirement','budget','order_status','total_tender','created_at','updated_at','deleted_at'];
+    protected $fillable = ['id','order_name','rent_application_id','rent_contract_id','inspect_id','issue_id','group_id','jobs','user_id','tenement_id','order_sn','order_type','rent_house_id','District','TA','Region','start_time','end_time','requirement','budget','order_status','total_tender','created_at','updated_at','deleted_at'];
 }
