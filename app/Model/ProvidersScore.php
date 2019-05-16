b206376ed9b5d4f34411864d72c934e30c0f9b74
supersonicgeng <13971394623@163.com>
2019-05-16 16:56:45 +0800

    调试bug

--- app/Model/ProvidersScore.php
+++ app/Model/ProvidersScore.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProvidersScore extends Model
 {
-    protected $table    = 'providers_order';
+    protected $table    = 'providers_score';
     protected $fillable = ['id','order_id','service_id','quality_score','community_score','money_score','score_detail','created_at','updated_at','deleted_at'];
 }
