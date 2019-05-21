0c8a50026d9145bc18ab4cd5b18b28ae9e667644
supersonicgeng <13971394623@163.com>
2019-05-21 17:54:54 +0800

    调试bug

--- app/Services/ProvidersService.php
+++ app/Services/ProvidersService.php
@@ -459,16 +459,15 @@ class ProvidersService extends CommonService
                 return $this->error('3','no more order info');
             }
             $res = $model->offset(($page-1)*5)->limit(5)->select('rent_house_id')->get();
-            dd($res);
-            foreach($res as $k=>$v){
-                $house_info[$k] = RentHouse::where('id',$v)->select('id','rent_category','property_name','property_type','address','available_time','rent_fee_pre_week','rent_least_fee','bedroom_no','bathroom_no','parking_no','garage_no','District','TA','Region','available_date','require_renter')->first()->toArray();
-                $house_info[$k]['house_pic'] = RentPic::where('rent_house_id',$v)->where('deleted_at',null)->pluck('house_pic')->toArray();// 图片
-                $house_info[$k]['full_address'] = $house_info[$k]['address'].','.Region::getName($house_info[$k]['District']).','.Region::getName($house_info[$k]['TA']).','.Region::getName($house_info[$k]['Region']); //地址
-            }
-            $data['house_info'] = $house_info;
-            $data['total_page'] = ceil($count/5);
-            $data['current_page'] = $page;
             if($res){
+                foreach($res as $k=>$v){
+                    $house_info[$k] = RentHouse::where('id',$v)->select('id','rent_category','property_name','property_type','address','available_time','rent_fee_pre_week','rent_least_fee','bedroom_no','bathroom_no','parking_no','garage_no','District','TA','Region','available_date','require_renter')->first()->toArray();
+                    $house_info[$k]['house_pic'] = RentPic::where('rent_house_id',$v)->where('deleted_at',null)->pluck('house_pic')->toArray();// 图片
+                    $house_info[$k]['full_address'] = $house_info[$k]['address'].','.Region::getName($house_info[$k]['District']).','.Region::getName($house_info[$k]['TA']).','.Region::getName($house_info[$k]['Region']); //地址
+                }
+                $data['house_info'] = $house_info;
+                $data['total_page'] = ceil($count/5);
+                $data['current_page'] = $page;
                 return $this->success('get house list success',$data);
             }else{
                 return $this->error('3','get house list failed');
