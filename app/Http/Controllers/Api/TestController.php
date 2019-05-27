dcf8ed38322c12698022ba7d29f3ac55ac654ade
supersonicgeng <13971394623@163.com>
2019-05-27 18:24:56 +0800

    调试PDF下载

---
+++ app/Http/Controllers/Api/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Mpdf\Mpdf;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+        $ip = "{$_SERVER['SERVER_NAME']}";
+        $dashboard_pdf_file = $ip."/Public/pdf/1.pdf";
+        $pdf = new Mpdf();
+        $pdf->SetImportUse();
+        $pagecount = $pdf->SetSourceFile($dashboard_pdf_file);
+        for ($i=1; $i<=$pagecount; $i++) {
+            $import_page = $pdf->ImportPage();
+            $pdf->UseTemplate($import_page);
+            if ($i < $pagecount)
+                $pdf->AddPage();
+        }
+        $pdf->Output();
+
+    }
+}
