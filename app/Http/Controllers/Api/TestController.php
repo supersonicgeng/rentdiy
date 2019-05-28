b9d989cf0c1b2bd8e255c61046ae6061764d92f6
supersonicgeng <13971394623@163.com>
2019-05-28 10:07:38 +0800

    调试 PDF

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Mpdf\Mpdf;
+use setasign\Fpdi\PdfParser\StreamReader;
 
 class TestController extends Controller
 {
