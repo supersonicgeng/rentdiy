656a9dc5aad70ba7a604889bcd5a9a963fb55234
supersonicgeng <13971394623@163.com>
2019-05-29 17:24:44 +0800

    调试PDF

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Mpdf\Mpdf;
 use setasign\Fpdi\PdfParser\StreamReader;
 
 class TestController extends Controller
@@ -13,6 +14,7 @@ class TestController extends Controller
         $ip = "{$_SERVER['SERVER_NAME']}";
         $dashboard_pdf_file = "http://".$ip."/pdf/4.pdf";
         $fileContent = file_get_contents($dashboard_pdf_file,'rb');
+        dd($fileContent);
         $mpdf = new Mpdf();
         $pagecount = $mpdf->setSourceFile(StreamReader::createByString($fileContent));
         for($i=1; $i<=$pagecount;$i++){
