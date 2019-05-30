eb332e24442a50de1e7f2904ef8f242e612bb954
supersonicgeng <13971394623@163.com>
2019-05-30 15:08:40 +0800

    调试bug

--- routes/api.php
+++ routes/api.php
@@ -58,7 +58,7 @@ Route::group(['namespace' => 'Api'], function (Router $router) {
     });
     // 测试
     $router->group(['prefix' => 'test'], function (Router $router){
-        $router->get('test','test@test'); // 测试 pdf 5.28
+        $router->get('test','TestController@test'); // 测试 pdf 5.28
     });
 });
 
