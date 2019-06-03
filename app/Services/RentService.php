985f9b1661b960eae248b012a7d196989c5540ed
supersonicgeng <13971394623@163.com>
2019-06-03 19:13:07 +0800

    调试

--- app/Services/RentService.php
+++ app/Services/RentService.php
@@ -1089,7 +1089,7 @@ class RentService extends CommonService
             return $this->error('2','this account is not a tenement role');
         }else{
             $model = new RentContract();
-            $contract_ids = ContractTenement::where('tenement_id',$input['tenement_id'])->select('contract_id')->get()->toArray();
+            $contract_ids = ContractTenement::where('tenement_id',$input['tenement_id'])->pluck('contract_id')->get()->toArray();
             dd($contract_ids);
             $contact_status = @$input['contact_status'];
             if($contact_status){
