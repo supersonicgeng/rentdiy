ce102691ee99ae652f90c4ce6e90fd24737caad8
supersonicgeng <13971394623@163.com>
2019-06-04 15:14:48 +0800

    调试bug

--- app/Services/RentService.php
+++ app/Services/RentService.php
@@ -1989,9 +1989,9 @@ class RentService extends CommonService
         //dd($input);
         $model = new ContractTenement();
         $res = $model->where('contract_id',$input['contract_id'])->pluck('tenement_id')->first();
-        $identification = TenementCertificate::where('tenement_id',$res)->select('certificate_category','certificate_no','certificate_pic1','certificate_pic2')->get();
-        dd($identification);
-        if($identification != []){
+        $identification_res = TenementCertificate::where('tenement_id',$res)->first();
+        if($identification_res){
+            $identification = TenementCertificate::where('tenement_id',$res)->select('certificate_category','certificate_no','certificate_pic1','certificate_pic2')->get();
             return $this->success('get identification success',$identification);
         }else{
             $data[0] = $model->where('contract_id',$input['contract_id'])->select('identification_no as certificate_no','identification_type as certificate_category')->first();
