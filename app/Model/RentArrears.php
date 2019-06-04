dcbc01ec5f6a752dee3cb247d35ad88bda273a5c
supersonicgeng <13971394623@163.com>
2019-06-04 15:18:27 +0800

    调试bug

--- app/Model/RentArrears.php
+++ app/Model/RentArrears.php
@@ -9,5 +9,5 @@ class RentArrears extends Model
     protected $table    = 'rent_arrears';
     protected $fillable = ['id','contract_id','contract_sn','user_id','rent_house_id','tenement_id','tenement_name','tenement_email','tenement_phone','arrears_type','property_name',
         'effect_date','arrears_fee','rent_circle','rent_times','is_pay','pay_fee','need_pay_fee','pay_date','number','unit_price','subject_code','tex',
-        'expire_date','discount','items_name','describe','created_at','updated_at','deleted_at'];
+        'expire_date','discount','items_name','describe','bond_status','created_at','updated_at','deleted_at'];
 }
