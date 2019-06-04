1c79a0ac65651d6616855b4e3579c5310dad8644
supersonicgeng <13971394623@163.com>
2019-06-04 16:58:00 +0800

    调试bug

--- app/Services/TenementService.php
+++ app/Services/TenementService.php
@@ -210,6 +210,7 @@ class TenementService extends CommonService
             if($tenement_info){
                 $certificate_model = new TenementCertificate();
                 $certificate_data = $certificate_model->where('tenement_id',$tenement_info->id)->get()->toArray();
+                dd($certificate_data);
                 foreach ($certificate_data as $k => $v){
                     $tenement_info['certificate_category'][$k] = $v['certificate_category'];
                     $tenement_info['certificate_no'][$k] = $v['certificate_no'];
