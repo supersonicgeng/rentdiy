3b8603c0b939dd48a29167c6eeb382e9ffa02cce
supersonicgeng <13971394623@163.com>
2019-06-06 17:47:34 +0800

    调试bug

--- app/Services/OperatorService.php
+++ app/Services/OperatorService.php
@@ -163,7 +163,7 @@ class OperatorService extends CommonService
             'end_date'          => @$input['end_date']?$input['end_date']:$operator_info->end_date,
             'email'             => @$input['email']?$input['email']:$operator_info->email,
             'phone'             => @$input['phone']?$input['phone']:$operator_info->phone,
-            'update_at'         => date('Y-m-d H:i:s',time())
+            'updated_at'         => date('Y-m-d H:i:s',time())
         ];
         $res = $model->where('user_id',$user_id)->where('id',$operator_id)->update($operator_data);
         static $error = 0;
