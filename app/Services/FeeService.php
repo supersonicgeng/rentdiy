535808f02f44398ec610ddcb2796e88b284a84cf
supersonicgeng <13971394623@163.com>
2019-06-10 15:27:25 +0800

    调试bug

--- app/Services/FeeService.php
+++ app/Services/FeeService.php
@@ -181,7 +181,7 @@ class FeeService extends CommonService
             static $paid_all = 0;
             static $rent_arrears_all = 0;
             static $other_arrears_all = 0;
-            $res = $model->where('user_id',$input['user_id'])->pluck('contract_id')->offset(($input['page']-1)*10)->limit(10)->groupBy('contract_id');
+            $res = $model->where('user_id',$input['user_id'])->offset(($input['page']-1)*10)->limit(10)->pluck('contract_id')->groupBy('contract_id');
             foreach ($res as $k => $v){
                 $fee_res = $model->where('contract_id',$v)->get()->toArray();
                 $fee_count = count($fee_res);
