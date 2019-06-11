da54228bbb5b8c7569876dad4f985937baa95aaf
supersonicgeng <13971394623@163.com>
2019-06-11 14:46:44 +0800

    调试bug

---
+++ app/Model/TenementScore.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TenementScore extends Model
+{
+    protected $table    = 'tenement_score';
+    protected $fillable = ['id','tenement_id','tenement_name','user_id','pay_score','hygiene_score','facility_score','detail','contract_id','accept_again','birthday','created_at','updated_at','deleted_at'];
+}
