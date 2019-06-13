c5bb0cb87bc8156be110fa9fd11f98a41dfd2495
supersonicgeng <13971394623@163.com>
2019-06-13 15:45:01 +0800

    调试bug

--- app/Services/FeeService.php
+++ app/Services/FeeService.php
@@ -315,6 +315,7 @@ class FeeService extends CommonService
                 return $this->error('2','no more fee information');
             }else{
                 $res = DB::table(DB::raw($sql))->offset(($input['page']-1)*10)->limit(10)->select('contract_id')->groupBy('contract_id')->get()->toArray();
+                dd($res);
                 foreach ($res as $k => $v){
                     $fee_res = RentArrears::where('contract_id',$v['contract_id'])->get()->toArray();
                     $fee_list[$k]['contract_sn'] = $fee_res[0]['contract_sn'];
