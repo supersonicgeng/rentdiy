e96457fef2fba38856686b488c8baf76d4e54bf6
supersonicgeng <13971394623@163.com>
2019-06-18 14:23:13 +0800

    调试bug

--- app/Services/FeeService.php
+++ app/Services/FeeService.php
@@ -1258,9 +1258,11 @@ class FeeService extends CommonService
     {
         $excel = new Excel();
         $data = $excel::toArray(new UsersImport(), $file);
+        dump($data);
         if($input['bank_type'] == 'ANZ'){
-            dump($data[0]);
+            dump(11);
             if($data[0][0] != 'Type' || $data[0][1] != 'Details' || $data[0][2] != 'Particulars' || $data[0][3] != 'Code' || $data[0][4] != 'Reference' || $data[0][5] != 'Amount' || $data[0][6] != 'Date' || $data[0][7] != 'ForeignCurrencyAmount' || $data[0][8] != 'ConversionCharge'){
+                dump(22);
                 return $this->error('2','the csv file is not the select bank');
             }else{
                 dump(11);
