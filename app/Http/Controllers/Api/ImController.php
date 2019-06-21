8decedc24e1e59ded33fe3c31145a0501f9a69f6
supersonicgeng <13971394623@163.com>
2019-06-21 15:53:10 +0800

    调试bug

--- app/Http/Controllers/Api/ImController.php
+++ app/Http/Controllers/Api/ImController.php
@@ -37,7 +37,9 @@ class ImController extends Controller
         if($group){// 查看该用户发送的消息
             $group = $group->toArray();
             foreach ($group as $k => $v){
-                $total_msg[] = Im::where('from',$user)->where('to',$v['to'])->get()->union(Im::where('to',$user)->where('from',$v['to'])->get())->toArray(); // 已发消息 和对方返回的消息
+                $send_msg = Im::where('from',$user)->where('to',$v['to']);
+                $recive_msg = Im::where('to',$user)->where('from',$v['to']);
+                $total_msg[] = $send_msg->union($recive_msg)->get()->toArray();// 已发消息 和对方返回的消息
                 $to[] = $v['to'];
             }
             $other_msg = Im::whereNotIn('from',$to)->where('to',$user)->groupBy('from')->get(); // 无回复的 消息列表
