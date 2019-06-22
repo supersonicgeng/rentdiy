2bc3c9560fafd9967dceeac1a50b355260e3dd2a
supersonicgeng <13971394623@163.com>
2019-06-22 14:45:43 +0800

    调试bug

--- app/Services/OperatorService.php
+++ app/Services/OperatorService.php
@@ -360,7 +360,7 @@ class OperatorService extends CommonService
         $operator_id = $input['operator_id'];
         $user_id = $input['user_id'];
         $room_list = OperatorRoom::where('operator_id',$operator_id)->pluck('house_id');
-        $res = $model->where('user_id',$user_id)->whereIn('id',$room_list)->where('deleted_at',null)->select('id','District','TA','Region','group_id','rent_category','property_name','property_type','rent_fee_pre_week','building_area','actual_area','pre_rent','least_rent_time','margin_rent','bedroom_no','bathroom_no','parking_no','garage_no','require_renter','short_words','rent_fee','rent_least_fee','can_party','can_pet','can_smoke','other_rule','address','lat','lon','available_date','is_put')->groupBy('group_id')->offset(($page-1)*9)->limit(9)->get()->toArray();
+        $res = $model->where('user_id',$user_id)->whereIn('id',$room_list)->where('deleted_at',null)->select('id','District','TA','Region','group_id','rent_category','property_name','property_type','rent_fee_pre_week','building_area','actual_area','pre_rent','least_rent_time','margin_rent','bedroom_no','bathroom_no','parking_no','garage_no','require_renter','short_words','rent_fee','rent_least_fee','can_party','can_pet','can_smoke','other_rule','address','lat','lon','available_date','is_put')->get()->toArray();
         if($res){
             foreach ($res as $k => $v){
                 $res[$k]['house_pic'] =  RentPic::where('rent_house_id',$v['id'])->where('deleted_at',null)->pluck('house_pic')->toArray();
