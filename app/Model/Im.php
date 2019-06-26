28709950cd8e1c9b8dced8fcaa531f9b1c4cc63d
supersonicgeng <13971394623@163.com>
2019-06-26 11:49:47 +0800

    调试bug

--- app/Model/Im.php
+++ app/Model/Im.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Im extends Model
 {
     protected $table    = 'im_info';
-    protected $fillable = ['id','from','to','msg','created_at','updated_at','deleted_at'];
+    protected $fillable = ['id','from','to','msg','created_at','is_read','updated_at','deleted_at'];
 }
