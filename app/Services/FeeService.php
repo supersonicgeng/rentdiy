22ba365960d6bc7d3710f05cf4c758ef5b76efa0
supersonicgeng <13971394623@163.com>
2019-09-05 19:01:15 +0800

    调试bug

--- app/Services/FeeService.php
+++ app/Services/FeeService.php
@@ -3500,8 +3500,8 @@ The above work has been completed, you can issue an invoice to the landlord..",
                     foreach ($fee_list as $k => $v){
                         $mpdf->WriteText(16+$k*10,'155',$v->items_name);
                         $mpdf->WriteText(42+$k*10,'155',$v->describe);
-                        $mpdf->WriteText(90+$k*10,'155',$v->rate);
-                        $mpdf->WriteText(115+$k*10,'155',(string)$v->number);
+                        $mpdf->WriteText(92+$k*10,'155',$v->rate);
+                        $mpdf->WriteText(118+$k*10,'155',(string)$v->number);
                         $mpdf->WriteText(138+$k*10,'155',$v->discount);
                         $mpdf->WriteText(158+$k*10,'155',$v->tex);
                         $mpdf->WriteText(175+$k*10,'155',$v->arrears_fee);
