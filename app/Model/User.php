ddfa2b72dca222839a89f990cf0e42ec7190b265
supersonicgeng <13971394623@163.com>
2019-09-09 15:57:56 +0800

    调试bug

--- app/Model/User.php
+++ app/Model/User.php
@@ -8,5 +8,5 @@ class User extends Model
 {
     protected $table    = 'user';
     protected $fillable = ['id','username','head_img','sexy','nickname','phone','facebook_id','google_id','wechat_openid','login_token','login_expire_time',
-        'user_role','house_number','jobs','email','password','created_at','updated_at','deleted_at'];
+        'user_role','house_number','jobs','email','balance','free_balance','password','created_at','updated_at','deleted_at'];
 }
