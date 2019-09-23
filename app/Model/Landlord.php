cbcd5347db038a2a8310c91c8a74e805ca2fe83c
supersonicgeng <13971394623@163.com>
2019-09-23 19:43:01 +0800

    调试bug

--- app/Model/Landlord.php
+++ app/Model/Landlord.php
@@ -9,10 +9,10 @@ class Landlord extends Model
     protected $table    = 'landlord_information';
     protected $fillable = ['id','user_id','landlord_sn','landlord_name','property_address','first_name','middle_name','last_name','headimg','assign_name','tax_no','phone','mobile','email',
         'bank_account','hm','wk','maill_address','mail_code','notice','subject_code','created_at','updated_at','deleted_at'];
-    protected $casts = [
+    /*protected $casts = [
         'phone'     => 'integer',
         'mobile'    => 'integer',
         'hm'        => 'integer',
         'wk'        => 'integer',
-    ];
+    ];*/
 }
--- app/Model/Providers.php
+++ app/Model/Providers.php
@@ -9,12 +9,12 @@ class Providers extends Model
     protected $table    = 'service_information';
     protected $fillable = ['id','user_id','providers_sn','service_name','first_name','middle_name','last_name','headimg','jobs','address','tax_no','phone','mobile','email','license_no',
         'bank_account','hm','wk','maill_address','mail_code','about_us','created_at','updated_at','deleted_at'];
-    protected $casts = [
+    /*protected $casts = [
         'phone'     => 'integer',
         'mobile'    => 'integer',
         'hm'        => 'integer',
         'wk'        => 'integer',
         'mail_code' => 'integer',
         'bank_account'  => 'integer',
-    ];
+    ];*/
 }
