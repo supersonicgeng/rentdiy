77c5f22f9d3dd8a3d1204e893c86514e96c7d736
supersonicgeng <13971394623@163.com>
2019-09-25 18:09:15 +0800

    调试bug

--- routes/admin/report.php
+++ routes/admin/report.php
@@ -7,5 +7,6 @@ Route::prefix('report')->namespace('Report')->name('report.')->group(function ()
     Route::get('/userDetail', 'ReportController@userDetail')->name('userDetail');//用户信息明细列表
     Route::get('/chargeList','ReportController@chargeList')->name('chargeList');//充值列表
     Route::get('/expenseList','ReportController@expenseList')->name('expenseList');//花费列表
-    Route::get('/landlordAnalyze','ReportController@landlordAnalyze')->name('landlordAnalyze');//用户运营分析
+    Route::get('/landlordAnalyze','ReportController@landlordAnalyze')->name('landlordAnalyze');//用户使用分析
+    Route::get('/landlordRunAnalyze','ReportController@landlordRunAnalyze')->name('landlordRunAnalyze');//用户运营分析
 });
