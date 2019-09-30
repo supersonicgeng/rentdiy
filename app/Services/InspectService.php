8a1a6dd21c039b0d21d8bddc217570101c11adc7
supersonicgeng <13971394623@163.com>
2019-09-30 10:44:57 +0800

    调试bug

--- app/Services/InspectService.php
+++ app/Services/InspectService.php
@@ -1888,7 +1888,7 @@ An inspection has been scheduled about date, please communicate with the tenant 
         $data['tenement_phone'] = ContractTenement::where('contract_id',$res->contract_id)->pluck('tenement_phone')->first();
         $data['tenement_email'] = ContractTenement::where('contract_id',$res->contract_id)->pluck('tenement_e_mail')->first();
         $data['landlord_name'] = RentContract::where('id',$res->contract_id)->pluck('landlord_full_name')->first();
-        $data['landlord_phone'] = RentContract::where('id',$res->contract_id)->pluck('landlord_phone')->first();
+        $data['landlord_phone'] = RentContract::where('id',$res->contract_id)->pluck('landlord_telephone')->first();
         $data['landlord_email'] = RentContract::where('id',$res->contract_id)->pluck('landlord_e_mail')->first();
         return $this->success('get message success',$data);
     }
