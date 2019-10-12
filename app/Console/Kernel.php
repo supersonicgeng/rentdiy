975860af9f7a580f7e2713e3e8f8c1c13e557754
supersonicgeng <13971394623@163.com>
2019-10-12 17:37:00 +0800

    调试bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use App\Http\Controllers\Api\PhoneController;
 use App\Models\CustomerInfo;
 use App\Models\OrderProfit;
+use App\Services\TaskService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Http\Controllers\Api\OrderController;
@@ -32,46 +33,24 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        //真实分佣
-        $schedule->call(function () {
-            $a = new OrderController();
-            $a->after();
-        })->monthlyOn(22, '00:05');
 
         $schedule->call(function () {
-            $a = new OrderController();
-            $a->after();
-        })->monthlyOn(22, '01:05');
+            $task = new TaskService();
+            $task->relet();
+            $task->testTask();
+            $task->checkInsurance();
+            $task->increaseRate();
+            $task->bondCheck();
+            $task->bondLodged();
+            $task->ticket();
+            $task->arrearsNote();
+            $task->landlordArrearsNote();
+        })->daily();
 
-
-        //定时拉取订单
         $schedule->call(function () {
-            $phoneOrder = new PhoneController();
-            $phoneOrder->getOrderList();
-            $phoneOrder->updateOrder();
-        })->everyMinute();
-
-        //月初清空 当月新增可提现金额
-        $schedule->call(function () {
-            CustomerInfo::query()->update(['withdraw_month' => 1]);
-        })->monthlyOn(1, '00:00');
-
-        //更新当月新增可提现金额
-        $schedule->call(function () {
-
-            $month_start = Carbon::now()->subMonth()->firstOfMonth();
-            $month_end = Carbon::now()->subMonth()->lastOfMonth();
-            $customer_infos = CustomerInfo::all();
-            //更新每个用户当月新增可提现金额
-            foreach ($customer_infos as $customer_info) {
-                $total = OrderProfit::where('f_id', $customer_info->customer_id)
-                    ->where('status', 1)->where('updated_at', [$month_start, $month_end])->sum('f_prof');
-                $customer_info->withdraw_month = $total;
-                $customer_info->save();
-            }
-
-
-        })->monthlyOn(25, '00:10');
+            $task = new TaskService();
+            $task->testTask();
+        })->dailyAt('17:50');
     }
 
     /**
